705d4f2fe4f515ceb0231300e27f6df5cd2ab747
franzrrrrr <imauel.steven35@smk.belajar.id>
2025-05-02 13:06:00 +0700

    first

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label>username:</label><br>
+        <input type="text" name="username"><br>
+        <label>password:</label><br>
+        <input type="password" name="password"><br>
+        <label>pesanan</label><br>
+        <input type="text" name="pesanan"><br>
+        <label>jumlah</label><br>
+        <input type="text" name="jumlah">
+        <input type="submit" value="Log in"><br>
+
+    </form>
+</body>
+</html>
+
+<?php
+
+    $name = $_POST['username'];    
+    $password = $_POST['password'];    
+    $pesanan = $_POST['pesanan'];    
+    $name = $_POST['username'];    
+
+    $profil = $name . "<br>". $password;
+
+    echo "$profil"
+
+?>
